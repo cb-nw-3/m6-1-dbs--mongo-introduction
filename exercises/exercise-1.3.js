dbea4902ede7826bd038762b556b9ededa20e019
donnyesq <adonis@darmas.ca>
2020-09-28 21:22:03 -0400

    exercise 1.3 complete

--- exercises/exercise-1.3.js
+++ exercises/exercise-1.3.js
@@ -7,19 +7,22 @@ const options = {
   useUnifiedTopology: true,
 };
 
-const getCollection = async (dbName) => {
+const getUsers = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
 
   await client.connect();
 
-  const db = client.db(dbName);
+  const db = client.db("exercise_1");
   console.log("connected!");
 
   const users = await db.collection("users").find().toArray();
   console.log("users", users);
 
+  if (users.length < 1) res.status(404);
+  else res.status(200).send(users);
+
   client.close();
   console.log("disconnected!");
 };
 
-getCollection("exercise_1");
+exports.getUsers = getUsers;
