5f3d0fb67ba12cc656cdeb93e6d0bbaa842cea79
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-09-28 21:45:18 -0400

    ex1.4 done

---
+++ exercises/exercise-1.4.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const addUser = async (req, res) => {
+  const { name } = req.body;
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("exercise_1");
+  console.log("connected!");
+
+  await db.collection("users").insertOne({ name });
+
+  res.status(201).json({ status: 201, data: req.body });
+
+  client.close();
+  console.log("disconnected!");
+};
+
+module.exports = { addUser };
