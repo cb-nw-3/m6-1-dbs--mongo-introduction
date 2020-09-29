49ce767a9d580196fc61bf4b6612c630f2bd1cc4
Mae <maeberris@outlook.com>
2020-09-29 17:00:53 -0400

    completed exercise 1

---
+++ exericises/exercise-1.1.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const dbFunction = async (dbName) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db(dbName);
+  console.log("connected!");
+
+  client.close();
+  console.log("disconnected");
+};
+
+dbFunction("exercise_1");
