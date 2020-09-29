18e72f4ddcf9ded06aa4d0d578bd1e69a900875e
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-09-28 22:31:10 -0400

    exercise-1.3

---
+++ exercises/exercise-1.3.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+console.log(MONGO_URI)
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const getUsers = async (req, res) => {
+    try {
+
+        const client = await MongoClient(MONGO_URI, options);
+
+        await client.connect();
+
+        const db = client.db('m6-1-mongo-introduction');
+        console.log("connected!");
+
+        const data = await db.collection("users").find().toArray();
+        console.log(data);
+
+        data.length
+            ? res.status(200).json({ status: 200, data })
+            : res.status(404).json({ status: 404, message: "No data!" });
+
+        client.close();
+        console.log("disconnected!");
+    } catch (err) {
+        console.log(err)
+    }
+
+};
+
+module.exports = { getUsers };
