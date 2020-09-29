f9b5b1ce520db0531215450a08a78e074df43c83
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2020-09-29 18:08:26 -0400

    Finished exercise 1

--- exercises/exercise-1.1.js
+++ exercises/exercise-1.1.js
@@ -4,25 +4,25 @@ require("dotenv").config();
 const { MONGO_URI } = process.env;
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const dbFunction = async (dbName) => {
-    console.log(MONGO_URI);
-    // creates a new client
-    const client = await MongoClient(MONGO_URI, options);
-    console.log(client);
-    // connect to the client
-    await client.connect();
+  // creates a new client
+  const client = await MongoClient(MONGO_URI, options);
 
-    // connect to the database (db name is provided as an argument to the function)
-    const db = client.db(dbName);
-    console.log("connected!");
+  // connect to the client
+  await client.connect();
 
-    // close the connection to the database server
-    client.close();
-    console.log("disconnected!");
+  // connect to the database (db name is provided as an argument to the function)
+  const db = client.db(dbName);
+  console.log("connected!");
+  await db.collection("users").insertOne({ name: "Buck Rogers" });
+
+  // close the connection to the database server
+  client.close();
+  console.log("disconnected!");
 };
 
-dbFunction('exercise_1');
+dbFunction("exercise_1");
