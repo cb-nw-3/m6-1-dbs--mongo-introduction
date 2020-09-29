3a8507d4156d612ccc0f829fae3ebabf4681d6af
donnyesq <adonis@darmas.ca>
2020-09-29 19:28:33 -0400

    exercise 2.3 complete

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -31,4 +31,25 @@ const createGreeting = async (req, res) => {
   console.log("disconnected!");
 };
 
+const getGreeting = async (req, res) => {
+  const _id = req.params._id;
+  console.log("id", _id);
+
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("exercise_1");
+  console.log("connected!");
+
+  const greeting = await db
+    .collection("greetings")
+    .findOne({ _id }, (err, result) => {
+      result
+        ? res.status(200).json({ status: 200, _id, data: result })
+        : res.status(404).json({ status: 404, _id, data: "Not Found" });
+      client.close();
+    });
+  console.log("disconnected!");
+};
+
 exports.createGreeting = createGreeting;
+exports.getGreeting = getGreeting;
