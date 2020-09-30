22fda99f9aca6e26ca9104fb6d1bccba431531f5
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-09-30 18:30:29 -0400

    tweak

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -9,8 +9,9 @@ const options = {
 };
 
 const createGreeting = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
   try {
-    const client = await MongoClient(MONGO_URI, options);
     await client.connect();
 
     const db = client.db("exercise_1");
@@ -20,12 +21,12 @@ const createGreeting = async (req, res) => {
     assert.strictEqual(1, r.insertedCount);
 
     res.status(201).json({ status: 201, data: req.body });
-    client.close();
-    console.log("disconnected!");
   } catch (err) {
     console.log(err.stack);
     res.status(500).json({ status: 500, data: req.body, message: err.message });
   }
+  client.close();
+  console.log("disconnected!");
 };
 
 module.exports = { createGreeting };
