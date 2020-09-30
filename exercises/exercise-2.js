d4f345a347d5a54ffb9916c19d564ef1198c328a
Mae <maeberris@outlook.com>
2020-09-30 13:28:55 -0400

    completed 2.1

---
+++ exercises/exercise-2.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const assert = require("assert");
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const createGreeting = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+
+    const db = client.db("exercises");
+    const r = await db.collection("greetings").insertOne(req.body);
+    assert.equal(1, r.insertedCount);
+
+    res.status(201).json({ status: 201, data: req.body });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+  client.close();
+};
+
+module.exports = { createGreeting };
