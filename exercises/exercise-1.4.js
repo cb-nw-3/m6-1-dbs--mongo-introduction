9967d09c49f65a7821ee2da459800f0ae44abc5d
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-30 19:25:47 -0400

    ex1-4 completed

---
+++ exercises/exercise-1.4.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const addUser = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+  console.log("connected!");
+
+  const db = client.db("exercise_1");
+
+  // add a new user to the collection
+  await db.collection("users").insertOne({ name: "Morty Smith" });
+
+  const data = await db.collection("users").find().toArray();
+  console.log("The users data is:", data);
+
+  data.length
+    ? res.status(201).json({ status: 201, data })
+    : res.status(404).json({ status: 404, message: "No data found!" });
+
+  client.close();
+  console.log("disconnected!");
+};
+
+module.exports = { addUser };
