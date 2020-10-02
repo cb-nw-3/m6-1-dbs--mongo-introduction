9ea4b347fc410b4ec6a99ec9d29594f45b6004b7
Truong Dao <truong.dao.do@gmail.com>
2020-10-02 17:51:36 -0400

    Solution Ex1.2

--- exercises/exercise-1.2.js
+++ exercises/exercise-1.2.js
@@ -1,36 +1,27 @@
-"use strict";
-
 const { MongoClient } = require("mongodb");
 
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
-// Missing Code for exercise-1.1
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-//
 
-// run from terminal(root) as node ".\exercises\exercise-1.1.js"
-
-console.log("The URI is:,", MONGO_URI);
-const dbFunction = async (dbName) => {
+const getCollection = async (dbName) => {
   // creates a new client
   const client = await MongoClient(MONGO_URI, options);
 
   // connect to the client
   await client.connect();
-
-  // connect to the database (dbName is provided as an argument to the function)
   const db = client.db(dbName);
-  console.log("connected!");
 
-  await db.collection("users").insertOne({ name: "Buck Rogers" });
+  const data = await db.collection("users").find().toArray();
+  console.log("This returns users from our collections", data);
 
   // close the connection to the database server
   client.close();
   console.log("disconnected!");
 };
 
-dbFunction("exercise_1");
+getCollection("exercise_1");
