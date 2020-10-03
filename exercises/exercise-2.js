083d3b1822f5708b87f96b8e07a2beec8ef54ac3
Truong Dao <truong.dao.do@gmail.com>
2020-10-03 15:35:21 -0400

    Solution Ex-2.3

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -34,4 +34,25 @@ const createGreeting = async (req, res) => {
   client.close();
 };
 
-module.exports = { createGreeting };
+const getGreeting = async (req, res) => {
+  // res.status(200).json("bacon");
+  const _id = req.params._id;
+  console.log("The language ID is:", _id);
+
+  //Create and connect to client
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+
+  //Access the database
+  const db = client.db("exercise_1");
+
+  //if the id search result exists then it should return data.
+  db.collection("greetings").findOne({ _id }, (err, result) => {
+    result
+      ? res.status(200).json({ status: 200, _id, data: result })
+      : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    client.close();
+  });
+};
+
+module.exports = { createGreeting, getGreeting };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
 const { addUser } = require("./exercises/exercise-1.4");
 
-const { createGreeting } = require("./exercises/exercise-2");
+const { createGreeting, getGreeting } = require("./exercises/exercise-2");
 
 const PORT = process.env.PORT || 8000;
 
@@ -25,6 +25,7 @@ express()
   //##################################  End  exercise 1
 
   //################################## Start exercise 2
+  .get("/exercise_2/greeting/:_id", getGreeting)
   .post("/exercise_2/greeting", createGreeting)
 
   // handle 404s
