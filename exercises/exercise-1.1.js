e52c49b6edf877a7ca7d48dec7d2aef991d2b17b
Alex Langevin <alex_langevin@hotmail.ca>
2020-10-06 16:40:52 -0400

    Exercise 1.1 and 1.2 completed

--- exercises/exercise-1.1.js
+++ exercises/exercise-1.1.js
@@ -5,13 +5,14 @@ const {MongoClient} =  require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
+//taken from exercise-1.2 Had an error otherwise...
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
 const dbFunction = async (dbName) => {
-  console.log(MONGO_URI);
+  // console.log(MONGO_URI);
   const client = await MongoClient(MONGO_URI, options);
   
   await client.connect();
