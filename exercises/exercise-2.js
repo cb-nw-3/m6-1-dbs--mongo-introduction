ee3c3e11480ac0de0050a012eb2d5915016bc087
Przemek Piwek <przemekpiwek@gmail.com>
2020-10-06 21:57:27 -0400

    Exercise 2.4

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -17,7 +17,6 @@ const createGreeting = async (req, res) => {
     const db = client.db("exercise_1");
     const r = await db.collection("greetings").insertOne(req.body);
     assert.equal(1, r.insertedCount);
-    console.log(assert.equal(50, 50));
     res.status(201).json({ status: 201, data: req.body });
   } catch (error) {
     console.log(error.stack);
@@ -26,4 +25,20 @@ const createGreeting = async (req, res) => {
   client.close();
 };
 
-module.exports = { createGreeting };
+const getGreeting = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  const { _id } = req.params;
+  await client.connect();
+  const db = client.db("exercise_1");
+  db.collection("greetings").findOne(
+    { _id: _id.toUppercase() },
+    (err, result) => {
+      result
+        ? res.status(200).json({ status: 200, _id, result })
+        : res.status(404).json({ status: 404 });
+    }
+  );
+  client.close();
+};
+
+module.exports = { createGreeting, getGreeting };
