bbb03787cce82587a3b1bf4efa2b073c83f95ca0
Alex Langevin <alex_langevin@hotmail.ca>
2020-10-07 15:01:44 -0400

    ex1.3

---
+++ exercises/exercise-1.3.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const {MongoClient} = require("mongodb");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getUsers = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+  const db = client.db("exercise_1");
+
+  const data = await db.collection("users").find().toArray();
+  console.log(data);
+
+  if(data.length > 0){
+    res.status(200).json({ status: 200, data });
+  } else {
+    res.status(404);
+  }
+
+  client.close();
+  console.log('disconnected');
+}
+
+module.exports =  {getUsers};
