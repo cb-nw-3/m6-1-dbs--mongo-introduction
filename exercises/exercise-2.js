c808f5ed8d0a00403a6a14b2b40d797d2b29bbd4
Alex Langevin <alex_langevin@hotmail.ca>
2020-10-10 16:36:31 -0400

    2.1 completed

---
+++ exercises/exercise-2.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const {MongoClient} = require("mongodb");
+const assert = require("assert");
+
+require("dotenv").config();
+const {MONGO_URI} = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+const createGreeting = async (req,res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("exercises");
+    console.log("connected");
+    await db.collection("greetings").insertOne(req.body);
+    res.status(201).json({ status: 201, data: req.body });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+  client.close();
+  console.log("closed");
+};
+
+module.exports = {createGreeting};
