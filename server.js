c73a3c1149c943ae5a2bd6d0df422c3a5c4ddd2d
splashley <ashleyjlhynes@gmail.com>
2020-10-13 18:48:00 -0400

    Exercise 2.1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
 const { addUser } = require("./exercises/exercise-1.4");
+const { createGreeting } = require("./exercises/exercise-2");
 
 const PORT = process.env.PORT || 8000;
 
@@ -18,6 +19,7 @@ express()
   // exercise 1
   .get("/exercise-1/users", getUsers)
   // exercise 2
+  .post("/exercise-2/greeting", createGreeting)
 
   // handle 404s
   .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
