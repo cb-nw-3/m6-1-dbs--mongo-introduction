209a46b011981d3886f0231d347727199a302b67
splashley <ashleyjlhynes@gmail.com>
2020-10-13 20:44:33 -0400

    Exercise 2.3

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -32,4 +32,19 @@ const createGreeting = async (req, res) => {
   console.log("disconnected!");
 };
 
-module.exports = { createGreeting };
+const getGreeting = async (req, res) => {
+  const { _id } = req.params;
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("exercise_1");
+  db.collection("greetings").findOne({ _id: _id }, (err, result) => {
+    result
+      ? res.status(200).json({ status: 200, _id, data: result })
+      : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    client.close();
+  });
+};
+
+module.exports = { createGreeting, getGreeting };
